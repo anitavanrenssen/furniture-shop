9ff98f507c41d83f8ae971547280ca4be0317c6a
anitavanrenssen <anita.p.ap70@gmail.com>
2022-06-02 00:45:00 +0200

    placement of smaller product imgs, product cards styling

--- src/js/home.js
+++ src/js/home.js
@@ -12,38 +12,173 @@ createApp({
         new Product(
           "001",
           "Chairs",
-          "../resources/chair-blue.jpg",
-          "Bluey Chair",
+          "../resources/chair-1-sm.jpg",
+          "Mabibi Dark Chair",
           "A soft comfortable chair with a rich blue color and oak legs",
           1495,
-          "/src/resources/chair-blue.jpg"
+          "/src/resources/chair-1-sm.jpg"
         ),
         new Product(
           "002",
           "Tables",
-          "../resources/table.jpg",
-          "Baobab Table",
+          "../resources/table-1-sm.jpg",
+          "Guma Dining Table",
           "A 6 seater table made from solid oak wood and stainless steel legs ",
           4695,
-          "/src/resources/table.jpg"
+          "/src/resources/table-1-sm.jpg"
         ),
         new Product(
           "003",
           "Couches",
           "../resources/brown-couch.jpg",
-          "Leather Couch",
+          "Mdumbi Leather Couch",
           "A 3 seater couch made from 100% cow leather and stainless steel legs",
-          12595,
+          21995,
           "/src/resources/brown-couch.jpg"
         ),
         new Product(
           "004",
           "Chairs",
-          "../resources/chair-brown.jpg",
-          "Browny Chair",
+          "../resources/chair-table-sm.jpg",
+          "Mabibi Light Chair",
           "A soft comfortable chair with a brown color and oak legs",
-          2295,
-          "/src/resources/chair-brown.jpg"
+          1495,
+          "/src/resources/chair-table-sm.jpg"
+        ),
+        new Product(
+          "005",
+          "Chairs",
+          "../resources/chair-2-sm.jpg",
+          "Ocean Dining Chair",
+          "A soft comfortable chair with a brown color and oak legs",
+          1395,
+          "/src/resources/chair-2-sm.jpg"
+        ),
+        new Product(
+          "006",
+          "Chairs",
+          "../resources/chair-5-sm.jpg",
+          "Adam Dining Chair",
+          "A soft comfortable chair with a brown color and oak legs",
+          1395,
+          "/src/resources/chair-5-sm.jpg"
+        ),
+        new Product(
+          "007",
+          "Chairs",
+          "../resources/chair-6-sm.jpg",
+          "Rattan Occasional Chair",
+          "A soft comfortable chair with a brown color and oak legs",
+          2995,
+          "/src/resources/chair-6-sm.jpg"
+        ),
+        new Product(
+          "008",
+          "Chairs",
+          "../resources/chair-3-sm.jpg",
+          "Rib Dining Chair",
+          "A soft comfortable chair with a brown color and oak legs",
+          1395,
+          "/src/resources/chair-3-sm.jpg"
+        ),
+        new Product(
+          "009",
+          "Chairs",
+          "../resources/chair-desk-sm.jpg",
+          "Emma Desk Chair",
+          "A soft comfortable chair with a brown color and oak legs",
+          1895,
+          "/src/resources/chair-desk-sm.jpg"
+        ),
+        new Product(
+          "010",
+          "Chairs",
+          "../resources/chair-4-sm.jpg",
+          "Lubanzi Chair",
+          "A soft comfortable chair with a brown color and oak legs",
+          1495,
+          "/src/resources/chair-4-sm.jpg"
+        ),
+        new Product(
+          "011",
+          "Couches",
+          "../resources/couch-2-sm.jpg",
+          "Harker Couch",
+          "A soft comfortable chair with a brown color and oak legs",
+          18795,
+          "/src/resources/couch-2-sm.jpg"
+        ),
+        new Product(
+          "012",
+          "Couches",
+          "../resources/couch-3-sm.jpg",
+          "Forester Couch",
+          "A soft comfortable chair with a brown color and oak legs",
+          14495,
+          "/src/resources/couch-3-sm.jpg"
+        ),
+        new Product(
+          "013",
+          "Couches",
+          "../resources/couch-4-sm.jpg",
+          "Coffee Leather Couch",
+          "A soft comfortable chair with a brown color and oak legs",
+          14495,
+          "/src/resources/couch-4-sm.jpg"
+        ),
+        new Product(
+          "014",
+          "Tables",
+          "../resources/table-3-sm.jpg",
+          "Okavango Dining Table",
+          "A 6 seater table made from solid oak wood and stainless steel legs ",
+          3395,
+          "/src/resources/table-3-sm.jpg"
+        ),
+        new Product(
+          "015",
+          "Tables",
+          "../resources/table-2-sm.jpg",
+          "3-Piece Puzzle Side Tables",
+          "A 6 seater table made from solid oak wood and stainless steel legs ",
+          1895,
+          "/src/resources/table-2-sm.jpg"
+        ),
+        new Product(
+          "016",
+          "Tables",
+          "../resources/table-5-sm.jpg",
+          "Ngepi Side Table",
+          "A 6 seater table made from solid oak wood and stainless steel legs ",
+          1225,
+          "/src/resources/table-5-sm.jpg"
+        ),
+        new Product(
+          "017",
+          "Tables",
+          "../resources/table-6-sm.jpg",
+          "Hairpin Coffee Table",
+          "A 6 seater table made from solid oak wood and stainless steel legs ",
+          2225,
+          "/src/resources/table-6-sm.jpg"
+        ),
+        new Product(
+          "018",
+          "Tables",
+          "../resources/table-4-sm.jpg",
+          "Violet Bedside Table",
+          "A 6 seater table made from solid oak wood and stainless steel legs ",
+          895,
+          "/src/resources/table-4-sm.jpg"
+        ),
+        new Product(
+          "019",
+          "Tables",
+          "../resources/table-7-sm.jpg",
+          "Daisy Bedside Table",
+          "A 6 seater table made from solid oak wood and stainless steel legs ",
+          995,
+          "/src/resources/table-7-sm.jpg"
         ),
       ],
       shoppingCartArray: [],
